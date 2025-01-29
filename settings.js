48c190766d4c54bd5c183ccdc15578c651ce8759
jacobidika <165697897+jacobidika@users.noreply.github.com>
2025-01-29 20:27:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Jacob-e8o', // Your name
+  ownerNumber: '91983983773', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
